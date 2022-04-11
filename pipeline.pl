6b3fe0f5c893c05933dd1729185360ce428f6d5e
JungHyeIn <54706837+fff0996@users.noreply.github.com>
2022-04-11 13:29:27 +0900

    Update pipeline.pl

--- pipeline.pl
+++ pipeline.pl
@@ -9,7 +9,7 @@ my @env = ('AL','SM','PA');
 
 for (my $i = 0; $i<=$#traits; $i=$i+1){
         for(my $j = 0 ; $j <= $#env;$j=$j+1){
-                `/BiO/hae/00_exome/phase2_44_half_study_design/phase2_ldsc_genetic_heritability/ldsc/ldsc.py --ref-ld-chr /BiO/hae/00_exome/phase2_44_half_study_design/phase2_ldsc_genetic_heritability/ldsc/eur_w_ld_chr/ --w-ld-chr /BiO/hae/00_exome/phase2_44_half_study_design/phase2_ldsc_genetic_heritability/ldsc/eur_w_ld_chr/ --rg /BiO/hae/00_exome/phase2_44_half_study_design/phase2_ldsc_genetic_heritability/ourGWAS/$traits[$i]/$traits[$i]_v2.sumstats.gz,/BiO/hae/00_exome/phase2_44_half_study_design/phase2_ldsc_genetic_heritability/ourGWAS/$env[$j]/$env[$j]_v2.sumstats.gz --out $traits[$i]_$env[$j]`;
+                `python /BiO/hae/00_exome/phase2_44_half_study_design/phase2_ldsc_genetic_heritability/ldsc/ldsc.py --ref-ld-chr /BiO/hae/00_exome/phase2_44_half_study_design/phase2_ldsc_genetic_heritability/ldsc/eur_w_ld_chr/ --w-ld-chr /BiO/hae/00_exome/phase2_44_half_study_design/phase2_ldsc_genetic_heritability/ldsc/eur_w_ld_chr/ --rg /BiO/hae/00_exome/phase2_44_half_study_design/phase2_ldsc_genetic_heritability/ourGWAS/$traits[$i]/$traits[$i]_v2.sumstats.gz,/BiO/hae/00_exome/phase2_44_half_study_design/phase2_ldsc_genetic_heritability/ourGWAS/$env[$j]/$env[$j]_v2.sumstats.gz --out $traits[$i]_$env[$j]`;
         }
 }
 
